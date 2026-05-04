4f35f88004292a43c1595989b6015cc5c8f92e09
Deependra Pal <deependrap412@gmail.com>
2026-05-04 23:04:55 +0530

    Add .env file for API key management and update api.js to use environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+const API_KEY = import.meta.env.VITE_API_KEY
 
 export async function fetchWeather(city) {
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=75d7efcb7b78218a52e2304533996930&units=metric`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
     );
 
     return response.data;
@@ -16,7 +17,7 @@ export async function fetchWeather(city) {
 export async function FetchForecast(City) {
    try{
     const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${City}&appid=75d7efcb7b78218a52e2304533996930&units=metric`,
+    `https://api.openweathermap.org/data/2.5/forecast?q=${City}&appid=${API_KEY}&units=metric`,
   );
 
   return response.data;
